4783605b8a450494fc73e3344ef1d7bc596b1abe
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2026-03-09 20:21:09 +0100

    Added dockerfile for hosting on server

--- data-visualization/next.config.ts
+++ data-visualization/next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   serverExternalPackages: ["adm-zip"],
   env: {
-    RESULTS_DIR: path.resolve(process.cwd(), "../results"),
+    RESULTS_DIR: process.env.RESULTS_DIR || path.resolve(process.cwd(), "../results"),
   },
 };
 
